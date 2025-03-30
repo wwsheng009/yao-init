fe584a89d6974635cbce9f343191d8e95c9d551c
wwsheng009 <vincentwwsheng@gmail.com>
2025-03-30 23:25:28 +0800

    refactor(assistants): 简化代码并移除冗余逻辑
    - 移除未使用的代码和注释，优化代码结构
    - 重构 `Create` 和 `Done` 函数，提升代码可读性和维护性
    - 添加对 `model_list` 函数的支持，并处理相关错误调用

--- assistants/model/src/index.ts
+++ assistants/model/src/index.ts
@@ -16,37 +16,8 @@ export function Create(
   input: neo.Message[],
   option: { [key: string]: any }
 ): neo.ResHookInit | null | string {
-  //case 1 return null
-  //return null
-
-  //case 2 returns a string,change the assistant_id
-  //return "assignment_id"
-  //check if the last message in input attachment with type URL
-
-  // Get the last message in the input array
   const lastMessage = input[input.length - 1];
-  input.pop();
 
-  // Check if the last message has attachments
-  if (lastMessage.attachments && lastMessage.attachments.length > 0) {
-    // Check if any attachment has the type 'URL'
-    // lastMessage.attachments.forEach((attachment) => {
-    //   if (attachment.type === 'URL' && attachment.url) {
-    //     // console.log('attachment');
-    //     // console.log(attachment);
-    //     try {
-    //       Send('读取网页' + attachment.url);
-
-    //       const content = getWebPageContent(attachment.url);
-    //       Send('读取网页完成' + attachment.url);
-
-    //       Send(content);
-    //     } catch (error) {
-    //       console.log(error.message);
-    //     }
-    //   }
-    // });
-  }
   // get the assistant_id from the message text where it looks  "@xxxxx"
   const mentenion_assistant_id = lastMessage.text?.match(/@(\w+)/)?.[1];
   let new_assistant_id = context.assistant_id;
@@ -58,21 +29,11 @@ export function Create(
     new_assistant_id = mentenion_assistant_id;
   }
 
-  input.push(lastMessage);
   //case 3 returns an object
   return {
     assistant_id: new_assistant_id, //optional,change the assistant_id,switch the assistant for following process
     chat_id: context.chat_id, //optional
-    // next: {
-    //   //optional, if you want to call another action in frontend
-    //   action: 'action1', //set to 'exit' to exit process
-    //   payload: {}
-    // },
-    input: input //optional,overwrite the input messages
-    // options: {
-    //   max_tokens: 8192
-    //   //optional, if you want to change the options for openai api call
-    // }
+    input: input //optional,修改 the input messages
   };
 }
 
@@ -83,38 +44,35 @@ export function Create(
  * @param output messages
  * @returns
  */
-function Done(
+export function Done(
   input: neo.ChatMessage[],
   output: neo.ChatMessage[]
 ): neo.ResHookDone | null {
-  // case 1 return null,no change
-  // return null
-  return null;
-  // case 2 return object
-  return {
-    next: {
-      action: 'action1', //set to 'exit' to exit stream,only set it when you want to exit the process
-      payload: {}
-    }
-    // output: output //change the output message
-  };
-}
+  if (
+    output.length > 0 &&
+    output[output.length - 1].props != null &&
+    output[output.length - 1].props['function'] !== ''
+  ) {
+    const lastLine = output[output.length - 1];
+    const funcName = lastLine.props['function'];
 
-/**
- * called every time when the call openai api failed,open ai return error messages
- *
- * @param input input messages
- * @param error error messages
- * @returns {next,input,output}
- */
-function Fail(input: neo.Message[], errror: string): neo.ResHookFail | null {
-  // case 1 return null,no change
-  // return null
-  return null;
-  // case 2 return object
-  return {
-    // output: output,
-    // most important, return error message
-    // error: 'error message'
-  };
+    if (funcName == 'model_list') {
+      Send(
+        {
+          text: '',
+          type: 'page',
+          props: {
+            url: '/index'
+          },
+          done: true
+        },
+        true
+      );
+    }
+    return {
+      output: [
+        { text: '错误的调用，不支持的函数调用：' + funcName }
+      ] as neo.ChatMessage[]
+    };
+  }
 }
